8fc53b3c30e5fd9dd8aa4821c2a7b2569baa7e73
George Karmelich <george.karmelich@gmail.com>
2018-03-19 12:24:03 -0400

    Add Basic GUI

--- assets/js/battle.jsx
+++ assets/js/battle.jsx
@@ -11,7 +11,7 @@ class Battle extends React.Component {
   constructor(props){
     super(props);
     this.channel = props.channel;
-    this.state = {skel: [], tilesMatched: 0, score: 0, paused: false, tilesSelected: 0};
+    this.state = {p1health: 100, p1energy: 0,  p2health: 100, p2energy: 0,};
 
     this.channel.join()
       .receive("ok", this.gotView.bind(this))
@@ -23,14 +23,52 @@ class Battle extends React.Component {
     this.setState(view.game);
   }
 
+
   render(){
 
 
     return (
     <div className="container">
       <h1>SAMPLE CONTENT - GAME WILL GO HERE!</h1>
+    <div className="players">  
+      <div className="player1">
+        <span><h2> Player 1 </h2></span>
+        <Button className="attack-btn"> Attack 1 </Button>
+        <Button className="attack-btn"> Attack 2 </Button>
+        <Button className="attack-btn"> Attack 3 </Button>
+        <Button className="attack-btn"> Attack 4 </Button>
+        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+          100
+        </div>
+        <br />
+        <p> Health </p>
+        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+          0
+        </div>
+        <p> Energy </p> 
+      </div>
+      <div className="player2">
+        <span><h2> Player 2 </h2></span>
+        <Button className="attack-btn"> Attack 1 </Button>
+        <Button className="attack-btn"> Attack 2 </Button>
+        <Button className="attack-btn"> Attack 3 </Button>
+        <Button className="attack-btn"> Attack 4 </Button>
+        <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+          100
+        </div>
+        <br />
+        <p> Health </p>        
+        <div class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+          0
+        </div>
+        <p> Energy </p>     
+      </div>
+
     </div>
-    )
-  };
+
+    </div>
+
+    );
+  }
 
 }
