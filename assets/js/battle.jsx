f99135ee4ed1eb4de0e5a7f40a61b6e47212ce8a
George Karmelich <george.karmelich@gmail.com>
2018-03-20 10:17:08 -0400

    game state rendering and backend

--- assets/js/battle.jsx
+++ assets/js/battle.jsx
@@ -11,7 +11,7 @@ class Battle extends React.Component {
   constructor(props){
     super(props);
     this.channel = props.channel;
-    this.state = {player1: [], player2: [], poke1: [], poke2: []};
+    this.state = {player1: [], player2: [], poke1: [], poke2: [], state: 1};
 
     this.channel.join()
       .receive("ok", this.gotView.bind(this))
@@ -23,49 +23,100 @@ class Battle extends React.Component {
     this.setState(view.game);
   }
 
+  //states: 0=waiting for second player; 1=player 1 turn; 2=player 2 turn; 
+  //3=game over
 
   render(){
+    if (this.state.state == 0) {
+    return (
+      <div className="container">
+         <br />
+         <br />
+         <h1> Waiting On Second Player..... </h1>
+      </div>
+    )}
+    else if (this.state.state == 1) {  
+    return (
+    <div className="container">
+    <div className="players">
+      <div className="player1">
+        <span><h2> Player 1: Your Move </h2></span>
+        <Button class="attack-btn"> Attack 1 </Button>
+        <Button class="attack-btn"> Attack 2 </Button>
+        <Button class="attack-btn"> Attack 3 </Button>
+        <Button class="attack-btn"> Attack 4 </Button>
+        <div class="space"></div>
+        <div className="progress-bar bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+          100
+        </div>
+        <br />
+        <p class="progress-label"> Health </p>
+        <div className="progress-bar bg-secondary" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+          0
+        </div>
+        <p class="progress-label"> Energy </p>
+      </div>
+      <div className="player2">
+        <span><h2> Player 2 </h2></span>
+        <br /><br />
+        <div class="space"></div>
+        <div className="progress-bar bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+          100
+        </div>
+        <br />
+        <p class="progress-label"> Health </p>
+        <div className="progress-bar bg-secondary" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+          0
+        </div>
+        <p class="progress-label"> Energy </p>
+      </div>
+
+    </div>
+
+    </div>
+
+    )}
+    else if (this.state.state == 2) {  
     return (
     <div className="container">
     <div className="players">
       <div className="player1">
         <span><h2> Player 1 </h2></span>
-        <Button className="attack-btn"> Attack 1 </Button>
-        <Button className="attack-btn"> Attack 2 </Button>
-        <Button className="attack-btn"> Attack 3 </Button>
-        <Button className="attack-btn"> Attack 4 </Button>
-        <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+        <br /><br />
+        <div class="space"></div>
+        <div className="progress-bar bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
           100
         </div>
         <br />
-        <p> Health </p>
-        <div className="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+        <p class="progress-label"> Health </p>
+        <div className="progress-bar bg-secondary" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
           0
         </div>
-        <p> Energy </p>
+        <p class="progress-label"> Energy </p>
       </div>
       <div className="player2">
-        <span><h2> Player 2 </h2></span>
-        <Button className="attack-btn"> Attack 1 </Button>
-        <Button className="attack-btn"> Attack 2 </Button>
-        <Button className="attack-btn"> Attack 3 </Button>
-        <Button className="attack-btn"> Attack 4 </Button>
-        <div className="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+        <span><h2> Player 2: Your Move </h2></span>
+        <Button class="attack-btn"> Attack 1 </Button>
+        <Button class="attack-btn"> Attack 2 </Button>
+        <Button class="attack-btn"> Attack 3 </Button>
+        <Button class="attack-btn"> Attack 4 </Button>
+        <div class="space"></div>
+        <div className="progress-bar bg-success" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
           100
         </div>
         <br />
-        <p> Health </p>
-        <div className="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
+        <p class="progress-label"> Health </p>
+        <div className="progress-bar bg-secondary" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">
           0
         </div>
-        <p> Energy </p>
+        <p class="progress-label"> Energy </p>
       </div>
 
     </div>
 
     </div>
 
-    );
+    )};
   }
 
 }
--- lib/pokemon/game.ex
+++ lib/pokemon/game.ex
@@ -7,14 +7,29 @@ defmodule Pokemon.Game do
       player2: [],
       poke1: [],
       poke2: [],
+      state: 0,
     }
   end
 
- def addPlayer(game) do
+ def addPlayer(game, player, pokemon) do
+   state = game.state
+   if player == 1 do
      %{
-       player1: [],
-       player2: [],
+       player1: game.player1 ++ [[pokemon, 100, 0]],
+       player2: game.player2,
+       poke1: game.poke1 ++ [[pokemon]],
+       poke2: game.poke2,
+       state: game.state
       }
+   else
+     %{
+       player1: game.player1,
+       player2: game.player2 ++ [[pokemon, 100, 0]],
+       poke1: game.poke1,
+       poke2: game.poke2 ++ [[pokemon]],
+       state: game.state
+      }
+    end
   end
 
   def client_view(game) do
@@ -36,23 +51,25 @@ defmodule Pokemon.Game do
        :attacks => %{:scratch => 10,
                      :lava_burn => 20,
                      :fire_fang => 30,
-                     :spl_inferno => 50}
+                     :spl_inferno => 50},
        :type => "Fire",
        :weakness => "Water"},
     %{:name => "Squirtle", :hp => 100, :energy => 100,
         :attacks => %{:tail_whip => 10,
                       :water_gun => 20,
                       :aqua_tail => 30,
-                      :spl_hydro_pump => 50}
+                      :spl_hydro_pump => 50},
         :type => "Water",
         :weakness => "Electric"},
     %{:name => "Bulbasaur", :hp => 100, :energy => 100,
        :attacks => %{:tackle => 10,
                      :vine_whip => 20,
                      :razor_leaf => 30,
-                     :spl_seed_bomb => 50}
+                     :spl_seed_bomb => 50},
       :type => "Grass",
       :weakness => "Fire"},
    ]
   end
+ 
+
 end
